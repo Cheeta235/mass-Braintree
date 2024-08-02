c31f44d599093c7e46ca10733c876c0ea0600af9
Cheeta235 <167458803+Cheeta235@users.noreply.github.com>
2024-08-02 17:44:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -315,7 +315,7 @@ def menu_callback(call):
 						bot.send_message(call.from_user.id, msgc)
 					else:
 						dd += 1
-					time.sleep(22)
+					time.sleep(24)
 		except Exception as e:
 			print(e)
 		stopuser[f'{id}']['status'] = 'start'
